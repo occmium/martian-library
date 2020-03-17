586c9b8128e842815a7afd4364f4b647dfd7b2ec
occmium <occkart@yahoo.com>
2020-03-17 17:06:25 +0600

    Database query optimization

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,7 +6,8 @@ module Types
           description: "Returns a list of items in the martian library"
 
     def items
-      Item.all
+      Item.includes(:user)
+      # Item.preload(:user)
     end
   end
 end
