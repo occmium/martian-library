ba296aa6d59caa555a4db658baeea9f5e9e3f724
occmium <occkart@yahoo.com>
2020-03-17 14:14:22 +0600

    Create first query

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,12 @@
-console.log('👻');
+// app/javascript/packs/index.js
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+import Library from '../components/Library';
+
+render(
+  <Provider>
+    <Library />
+  </Provider>,
+  document.querySelector('#root')
+);
