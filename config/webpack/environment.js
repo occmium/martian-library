740b31cc0233ffe07f39e54516b08adf3dbd33f3
occmium <occkart@yahoo.com>
2020-03-17 18:25:22 +0600

    Configure loader for Webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+// To make Webpack “understand” .gql files we need to configure a specific loader
+environment.loaders.append("graphql", {
+  test: /\.(graphql|gql)$/,
+  exclude: /node_modules/,
+  loader: "graphql-tag/loader"
+});
+
+module.exports = environment;
